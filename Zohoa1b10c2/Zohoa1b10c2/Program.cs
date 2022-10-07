62ac4ba9fb1724387a683be3ecc0b5f8554f430b
KUMAR V <thirumani.kumar-v@siemens.com>
2022-10-07 21:32:10 +0530

    Zoho Last cHANCE

--- Zohoa1b10c2/Zohoa1b10c2/Program.cs
+++ Zohoa1b10c2/Zohoa1b10c2/Program.cs
@@ -11,36 +11,81 @@ namespace Zohoa1b10c2
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
+            int length1 = input.Length;
             string num = "";
+            
             char character = '\0';
+            int c = 0;
             foreach(char i in input)
             {
-                if(i>='a'&& i<= 'z'){
-                    if (character != '\0' && num!="")
-                    {
-                        String s =character.Multiply(int.Parse(num)) ;
-                        Console.Write(s);
-                        character = i;
-                        num = "";
-                        
-                    }
+                c += 1;
 
-                    else
-                    {
-                        character = i;
-
-                    }
-
-                }
-
-                if(i>='1' && i < '9')
+                if(i>='0' && i <= '9')
                 {
                     num += i;
+                }
+                if (c == length1)
+                {
+                    int n = int.Parse(num);
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(character);
+
+                    }
+                    break;
 
                 }
+                if (i >= 'a' && i <='z')
+                {
+                    if (num != ""){
+                        int n = int.Parse(num);
+                        for(int j = 0; j < n; j++)
+                        {
+                            Console.Write(character);
+
+                        }
+                        character = i;
+                        num = "";
+
+                    }
+                    character = i;
+
+
+
+
+                    if (num=="")
+                    {
+                        continue;
+
+                    }
+                    
+                }
+                //if(i>='a'&& i<= 'z'){
+                //    if (character != '\0' && num!="")
+                //    {
+                //        String s =character.Multiply(int.Parse(num)) ;
+                //        Console.Write(s);
+                //        character = i;
+                //        num = "";
+                        
+                //    }
+
+                //    else
+                //    {
+                //        character = i;
+
+                //    }
+
+                //}
+
+                //if(i>='1' && i < '9')
+                //{
+                //    num += i;
+
+                //}
 
             }
-            Console.Write(character.Multiply(int.Parse(num)));
+            //Console.Write(character.Multiply(int.Parse(num)));
 
 
 
